96324331f949ab85637bc093576a56a3ab4fd953
thmsVDC <thomasvictor2909@gmail.com>
2025-02-07 16:08:32 -0300

    setup base flask structure

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from app import create_app, db
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return 'MoneyWise!'
+    return "MoneyWise!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
