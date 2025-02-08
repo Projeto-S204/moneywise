b5507d3d5e232d9538e01a5e42a64eb57c68fa7a
thmsVDC <thomasvictor2909@gmail.com>
2025-02-08 20:08:25 -0300

    Setup Home Page and Config.py

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
 import os
+from socket import socket
 
+BASEDIR = os.path.dirname(os.path.abspath("main.py"))
+DATABASE = "sqlite:///" + os.path.join(BASEDIR, "database.db")
+
+def find_available_port():
+    with socket() as s:
+        s.bind(("", 0))
+        print(s.getsockname())
+        return s.getsockname()[1]
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql:///moneywise_db"
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    FLASK_DEBUG = 1
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = DATABASE
+    SECRET_KEY = "k\x8d-\xbd\xb9\x05\xeax\x92\xd9{H\xf0\x9c\xf9\xde\x91\xc6\xe6\xa8\x14\xf9\x89t"
