828f5129343053536a840179d231379bca0d0f62
thmsVDC <thomasvictor2909@gmail.com>
2025-03-06 17:16:57 -0300

    revertendo mudancas do deploy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from app import create_app
+from config import Config
+
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, port=Config.find_available_port())
--- config.py
+++ config.py
@@ -1,16 +1,39 @@
+from socket import socket
 import psycopg2
-import os
+
+DB_HOST = "localhost"
+DB_NAME = "moneywise"
+DB_USER = "postgres"
+DB_PASS = "1234"
+DB_PORT = "5432"
+
 
 class Config:
-    SECRET_KEY = "k\x8d-\xbd\xb9\x05\xeax\x92\xd9{H\xf0\x9c\xf9\xde\x91\xc6\xe6\xa8\x14\xf9\x89t"
-    
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-        
+    SQLALCHEMY_DATABASE_URI = (
+        'postgresql://postgres:meritopg@localhost/dashboard'
+    )
+    SECRET_KEY = (
+        "k\x8d-\xbd\xb9\x05\xeax\x92\xd9{H\xf0\x9c\xf9\xde\x91\xc6\xe6"
+        "\xa8\x14\xf9\x89t"
+    )
+
+    JWT_SECRET_KEY = SECRET_KEY
+
+    @staticmethod
+    def find_available_port():
+        with socket() as s:
+            s.bind(("", 0))
+            return s.getsockname()[1]
+
     @staticmethod
     def get_db_connection():
-        database_url = SQLALCHEMY_DATABASE_URI
-        
-        conn = psycopg2.connect(database_url)
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            port=DB_PORT
+        )
         conn.autocommit = True
         return conn
