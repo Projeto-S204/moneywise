26bc1f85b7e4825fd94c9bcd469e4d72bfa04264
thmsVDC <thomasvictor2909@gmail.com>
2025-03-13 17:59:31 -0300

    ajustes na home page

--- app.py
+++ app.py
@@ -5,7 +5,4 @@ from config import Config
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=False, port=Config.find_available_port())
-
