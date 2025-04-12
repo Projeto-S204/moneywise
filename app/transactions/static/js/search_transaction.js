e0cd0193818cab9e5d0cbb9ad56312053935738f
pedrinho123321 <vasconcelosp43@gmail.com>
2025-04-12 14:16:04 -0300

    subindo a tela de contato

--- app/transactions/static/js/search_transaction.js
+++ app/transactions/static/js/search_transaction.js
@@ -1,42 +1,52 @@
 function searchTransaction() {
-  var input, filter, transactions, transactionItems, i, txtValue, found = false;
-  
+  var input,
+    filter,
+    transactions,
+    transactionItems,
+    i,
+    txtValue,
+    found = false;
+
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
-  
+
   transactions = document.querySelector(".transactions");
   if (!transactions) return;
 
   transactionItems = transactions.getElementsByClassName("transaction-item");
 
   for (i = 0; i < transactionItems.length; i++) {
-      let titleElement = transactionItems[i].querySelector(".transaction-name");
-      
-      if (titleElement) {
-          txtValue = titleElement.textContent || titleElement.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              transactionItems[i].style.display = "";
-              found = true;
-          } else {
-              transactionItems[i].style.display = "none";
-          }
+    let titleElement = transactionItems[i].querySelector(".transaction-name");
+
+    if (titleElement) {
+      txtValue = titleElement.textContent || titleElement.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        transactionItems[i].style.display = "";
+        found = true;
+      } else {
+        transactionItems[i].style.display = "none";
       }
+    }
   }
 
   let noResultMessage = document.getElementById("no-transaction-message");
   if (!noResultMessage) {
-      noResultMessage = document.createElement("p");
-      noResultMessage.id = "no-transaction-message";
-      noResultMessage.style.color = "white";
-      noResultMessage.style.textAlign = "center";
-      noResultMessage.textContent = "Nenhuma transação encontrada";
-      transactions.appendChild(noResultMessage);
+    noResultMessage = document.createElement("p");
+    noResultMessage.id = "no-transaction-message";
+    noResultMessage.style.color = "white";
+    noResultMessage.style.textAlign = "center";
+    noResultMessage.textContent = "Nenhuma transação encontrada";
+    transactions.appendChild(noResultMessage);
   }
 
   noResultMessage.style.display = found ? "none" : "block";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("search").addEventListener("keyup", searchTransaction);
-  document.getElementById("search").addEventListener("keyup", setTransactionAmount);
+  document
+    .getElementById("search")
+    .addEventListener("keyup", searchTransaction);
+  document
+    .getElementById("search")
+    .addEventListener("keyup", setTransactionAmount);
 });
--- app/transactions/static/js/transaction_form_page.js
+++ app/transactions/static/js/transaction_form_page.js
@@ -1,23 +1,31 @@
 function setTransactionType(type) {
-  document.getElementById('transaction_type').value = type;
+  document.getElementById("transaction_type").value = type;
 }
 
 function changeButtonColor(button) {
-  document.querySelectorAll('.transaction-type-buttons button').forEach(btn => {
-    btn.classList.remove('active');
-    btn.style.backgroundColor = '';
-    btn.style.color = btn.classList.contains('income-btn') ? '#92CA7E' : '#CA7E7E';
-  });
+  document
+    .querySelectorAll(".transaction-type-buttons button")
+    .forEach((btn) => {
+      btn.classList.remove("active");
+      btn.style.backgroundColor = "";
+      btn.style.color = btn.classList.contains("income-btn")
+        ? "#92CA7E"
+        : "#CA7E7E";
+    });
 
   if (button) {
-    button.classList.add('active');
-    button.style.backgroundColor = button.classList.contains('income-btn') ? '#92CA7E' : '#CA7E7E';
-    button.style.color = '#18181B';
+    button.classList.add("active");
+    button.style.backgroundColor = button.classList.contains("income-btn")
+      ? "#92CA7E"
+      : "#CA7E7E";
+    button.style.color = "#18181B";
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const transactionType = document.getElementById("transaction_type").value.trim();
+  const transactionType = document
+    .getElementById("transaction_type")
+    .value.trim();
 
   if (transactionType === "income") {
     changeButtonColor(document.getElementById("income-btn"));
@@ -27,16 +35,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function toggleNumberOfPayments() {
-  var checkbox = document.getElementById('checkbox');
-  var numberOfPayments = document.getElementById('number-of-payments-container');
-  var numberOfPaymentsInput = document.getElementById('number-of-payments-container');
+  var checkbox = document.getElementById("checkbox");
+  var numberOfPayments = document.getElementById(
+    "number-of-payments-container"
+  );
+  var numberOfPaymentsInput = document.getElementById(
+    "number-of-payments-container"
+  );
 
   if (checkbox.checked) {
-    numberOfPayments.style.display = 'block';
-    numberOfPaymentsInput.setAttribute('required', 'required');
+    numberOfPayments.style.display = "block";
+    numberOfPaymentsInput.setAttribute("required", "required");
   } else {
-    numberOfPayments.style.display = 'none';
-    numberOfPaymentsInput.removeAttribute('required');
+    numberOfPayments.style.display = "none";
+    numberOfPaymentsInput.removeAttribute("required");
   }
 }
 
@@ -44,12 +56,14 @@ window.onload = function () {
   toggleNumberOfPayments();
 };
 
-document.getElementById('checkbox').addEventListener('change', toggleNumberOfPayments);
+document
+  .getElementById("checkbox")
+  .addEventListener("change", toggleNumberOfPayments);
 
-document.querySelectorAll('select option').forEach(option => {
+document.querySelectorAll("select option").forEach((option) => {
   if (option.disabled) {
-    option.style.color = 'gray';
+    option.style.color = "gray";
   } else {
-    option.style.color = 'white';
+    option.style.color = "white";
   }
 });
