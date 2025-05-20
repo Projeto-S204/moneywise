c672d877c2c351250ed88b8fce596ac8de982dc9
thmsVDC <thomasvictor2909@gmail.com>
2025-05-20 19:09:55 -0300

    Merge branch 'feat-implementing-statistics-page-design' of github.com:Projeto-S204/moneywise into feat-implementing-statistics-page-design

--- tests/page-objects/profile_page.ts
+++ tests/page-objects/profile_page.ts
@@ -7,30 +7,9 @@ export class ProfilePage {
     this.page = page;
   }
 
-  async gotoTransactionsPage() {
-    await this.page.getByRole('link', { name: 'Transações' }).click();
-  }
-
-  async expectURL(currentPage: string) {
-    const CURRENT_URL = await this.page.url();
-    expect(CURRENT_URL).toContain(currentPage);
-  }
-
   async deleteUser(password: string) {
     await this.page.getByRole('link', { name: 'Deletar Conta' }).click();
     await this.page.getByRole('textbox', { name: 'Confirme sua senha:' }).fill(password);
     await this.page.getByRole('button', { name: 'Excluir' }).click();
-  }
-
-  async editProfile() {
-      await this.page.getByRole('button', { name: '' }).click();
-      await this.page.getByRole('textbox').first().fill('teste editado');
-      await this.page.getByRole('textbox').nth(1).fill('teste.editado@teste.com');
-      await this.page.getByRole('textbox').nth(2).fill('29/09/2010');
-      this.page.once('dialog', dialog => {
-        console.log(`Dialog message: ${dialog.message()}`);
-        dialog.dismiss().catch(() => {});
-      });
-      await this.page.getByRole('button', { name: 'Salvar' }).click();
   }
 }
